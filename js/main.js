cbfc1dd03577573bba4791c62b7f5e1af6a62a32
mdsurfer <nerik85@gmail.com>
2020-03-19 18:21:57 +0200

    hw2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+
+
 const searchForm = document.querySelector('.form-search'),
    inputCitiesFrom = document.querySelector('.input__cities-from'),
    dropdownCitiesFrom = document.querySelector('.dropdown__cities-from'),
@@ -5,27 +7,47 @@ const searchForm = document.querySelector('.form-search'),
    dropdownCitiesTo = document.querySelector('.dropdown__cities-to'),
    inputDateDepart = document.querySelector('.input__date-depart');
 
-console.log(inputCitiesFrom);
+let cities = [],
+  originCity = "SVX",
+  destinationCity = "KGD",
+  departDate = "2020-05-25",
+  one_way = false;
 
-const cities = ['Москва', 'Санкт-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск', 'Омск', 'Самара', 'Ростов-на-Дону', 'Уфа', 'Красноярск', 'Воронеж', 'Пермь', 'Волгоград'];
 
+
+const getData = (url, callback) => {
+   const request = new XMLHttpRequest();
+   request.open('GET', url);
+   request.addEventListener('readystatechange', () => { 
+      if (request.readyState !== 4) return; 
+      if (request.status === 200) {
+         callback(request.response);
+      } else {
+         console.error(request.status);
+      }
+   });
+   request.send();
+}
+const apiCityList = 'data/cities.json',
+   proxy = 'https://cors-anywhere.herokuapp.com/',
+   API_KEY = 'aa4b1c9709b896e36bbafde12dada32f',
+   API_CAL = 'http://min-prices.aviasales.ru/calendar_preload';
+   
 const showCity = (input, list) => {
-   dropdownCitiesFrom.textContent = '';
+   list.textContent = '';
+
 
    if (input.value !== '') {
-
       const filterCity = cities.filter((item) => {
-         const fixItem = item.toLowerCase();
-         return item.includes(input.value.toLowerCase());
+            const fixItem = item.name.toLowerCase();
+            return fixItem.includes(input.value.toLowerCase());
       });
 
       filterCity.forEach((item) => {
-
          const listItem = document.createElement('li');
          listItem.classList.add('dropdown__city');
-         listItem.textContent = item;
+         listItem.textContent = item.name;
          list.append(listItem);
-         console.log(listItem);
       });
 
    }
@@ -38,7 +60,6 @@ const selectCity = (event, input, list) => {
       console.log(target.textContent);
       input.value = target.textContent; //inputCities
       list.textContent = ''; //dropdownCities
-
    }
 }
    
@@ -46,11 +67,9 @@ inputCitiesFrom.addEventListener('input', () => {
    showCity(inputCitiesFrom, dropdownCitiesFrom)
 });
 
-
 dropdownCitiesFrom.addEventListener('click', () => {
    //console.log(event);
    selectCity(event, inputCitiesFrom, dropdownCitiesFrom);
-   
 });
 
 dropdownCitiesTo.addEventListener('click', () => {
@@ -63,4 +82,18 @@ inputCitiesTo.addEventListener('input', () => {
    showCity(inputCitiesTo, dropdownCitiesTo)
 });
 
+getData(apiCityList, (data) => {
+   
+   cities = JSON.parse(data).filter(item => item.name);
+   console.log(cities);
+});
 
+getData('http://min-prices.aviasales.ru/calendar_preload?origin=' + originCity + '&destination=' + destinationCity + '&depart_date=' + departDate + '&one_way=' + one_way,
+  data => {
+     const flightData = JSON.parse(data);
+     
+     
+    console.log(flightData);
+    return true;
+  }
+);
